671a17a0d8ad8a84e329d1a12077f3e2d790f650
Thorminate <thorwaag11@gmail.com>
2026-06-25 01:59:26 +0200

    fix release.yml (again x4)

---
+++ vscode-extension/esbuild.js
@@ -0,0 +1,11 @@
+﻿const { build } = require('esbuild');
+
+build({
+  entryPoints: ['src/extension.ts'],
+  bundle: true,
+  outfile: 'out/extension.js',
+  external: ['vscode'],
+  format: 'cjs',
+  platform: 'node',
+  minify: true,
+}).catch(() => process.exit(1));
